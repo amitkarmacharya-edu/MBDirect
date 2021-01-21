929262830485b7e7f57eb15ae16888a8b38e8d2c
Cesar Martinez <chernanma@msn.com>
2021-01-21 01:55:40 -0500

    Updating Server API routes for Login and Register

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const passport = require("./config/passport");
 const fileupload = require("express-fileupload"); 
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3001; 
 
 const db = require("./models");
 const app = express();
@@ -18,35 +18,43 @@ app.use(express.json());
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(fileupload());
 
-
-app.use(routes);
-
-app.post("/login", (req, res) => {
-  console.log(req.body);
-});
 
 app.post("/register", (req, res) => {
-  db.User
-      .create(req.body)
-      .then(dbUser => {
-        res.json(dbUser);
-        console.log(dbUser)
-      })
-      .catch(err => res.status(422).json(err));  
-});
-app.post("/user", (req, res) => {
-  
+  console.log(req.body);
+  db.User.create(req.body)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.status(401).json(err);
+    });
 });
 
+app.get("/users", (req, res) => {
+  // Here we add an "include" property to our options in our findAll query
+  db.User.findAll({}).then(dbUser => {
+    res.json(dbUser);
+  });
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  db.User.create(req.body)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.status(401).json(err);
+    });
+});
+
+app.use(routes);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      "==> :earth_americas:  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
