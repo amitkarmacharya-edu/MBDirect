9ce927a779d40fd70e3703ed5fe1d3ebeaee74dd
Cesar Martinez <cesaguma@hotmail.com>
2021-01-20 20:23:54 -0500

    Creating the client directory to use React.

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,51 +1,21 @@
 import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
-import { Link } from "react-router-dom";
+import DeleteBtn from "../components/DeleteBtn";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Books() {
-  const [books, setBooks] = useState([])
-  const [formObject, setFormObject] = useState({})
+  // Initialize books as an empty array
+    const [books, setBooks] = useState([]);
+  
+    useEffect(() => {
+      loadBooks();
+    }, []);
 
-  useEffect(() => {
-    loadBooks()
-  }, [])
-
-  function loadBooks() {
-    API.getBooks()
-      .then(res => 
-        setBooks(res.data)
-      )
-      .catch(err => console.log(err));
-  };
-
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then(res => loadBooks())
-      .catch(err => console.log(err));
-  }
-
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-  };
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
+    function loadBooks() {
+      // Add code here to get all books from the database and store them using setBooks
     }
-  };
 
     return (
       <Container fluid>
@@ -55,27 +25,10 @@ function Books() {
               <h1>What Books Should I Read?</h1>
             </Jumbotron>
             <form>
-              <Input
-                onChange={handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <Input
-                onChange={handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                onChange={handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              />
-              <FormBtn
-                disabled={!(formObject.author && formObject.title)}
-                onClick={handleFormSubmit}
-              >
-                Submit Book
-              </FormBtn>
+              <Input name="title" placeholder="Title (required)" />
+              <Input name="author" placeholder="Author (required)" />
+              <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
+              <FormBtn>Submit Book</FormBtn>
             </form>
           </Col>
           <Col size="md-6 sm-12">
@@ -86,12 +39,12 @@ function Books() {
               <List>
                 {books.map(book => (
                   <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
+                    <a href={"/books/" + book._id}>
                       <strong>
                         {book.title} by {book.author}
                       </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                    </a>
+                    <DeleteBtn />
                   </ListItem>
                 ))}
               </List>
@@ -103,6 +56,5 @@ function Books() {
       </Container>
     );
   }
-
 
 export default Books;
