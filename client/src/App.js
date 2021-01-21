23e7f3ebb75dffea0be56053d3e75f808e0f50e4
Cesar Martinez <cesaguma@hotmail.com>
2021-01-21 16:29:43 -0500

    Updating App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React , { useState }from "react";
 import Axios from 'axios';
 import API from './utils/API';
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import RegistrationForm from "./pages/RegistrationForm/RegirtrationForm";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute';
+
 // import Books from "./pages/Books";
 // import Nav from "./components/Nav";
 
@@ -43,27 +49,42 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div>
-        <h1>Register</h1>
-        <input placeholder='first_name' onChange={e=> setRegisterFirstName(e.target.value)}/>
-        <input placeholder='last_name' onChange={e=> setRegisterLastName(e.target.value)}/>
-        <input placeholder='email' onChange={e=> setRegisterUsername(e.target.value)}/>        
-        <input placeholder='password' onChange={e=> setRegisterPassword(e.target.value)}/>  
-        <button onClick={register}>Submit</button>      
+    <Router>
+      <div className="App">
+        <Header />
+          <div className="container d-flex align-items-center flex-column">
+            <Switch>
+              <Route path="/" exact={true}>
+                <RegistrationForm />
+              </Route>
+              <PrivateRoute path="/home">
+                <Home/>
+            </PrivateRoute>
+            </Switch>
+        </div>
+          <div>
+            <h1>Register</h1>
+            <input placeholder='first_name' onChange={e=> setRegisterFirstName(e.target.value)}/>
+            <input placeholder='last_name' onChange={e=> setRegisterLastName(e.target.value)}/>
+            <input placeholder='email' onChange={e=> setRegisterUsername(e.target.value)}/>        
+            <input placeholder='password' onChange={e=> setRegisterPassword(e.target.value)}/>  
+            <button onClick={register}>Submit</button>      
+          </div>
+          <div>
+            <h1>Login</h1>
+            <input placeholder='username' onChange={e=> setLoginUsername(e.target.value)}/>
+            <input placeholder='password' onChange={e=> setLoginPassword(e.target.value)}/>  
+            <button onClick={login}>Submit</button>      
+          </div>
+          <div>
+            <h1>Get User</h1>       
+            <button onlick={getuser}>Submit</button>      
+          </div>
+      
       </div>
-      <div>
-        <h1>Login</h1>
-        <input placeholder='username' onChange={e=> setLoginUsername(e.target.value)}/>
-        <input placeholder='password' onChange={e=> setLoginPassword(e.target.value)}/>  
-        <button onClick={login}>Submit</button>      
-      </div>
-      <div>
-        <h1>Get User</h1>       
-        <button onlick={getuser}>Submit</button>      
-      </div>
+
+    </Router>
     
-    </div>
   );
 }
 
