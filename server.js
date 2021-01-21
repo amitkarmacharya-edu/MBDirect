df6064beb59cceb77293f9d63f40c21de3fbe224
Cesar Martinez <chernanma@msn.com>
2021-01-20 21:48:07 -0500

    Updating server.js

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const routes = require("./routes");
+
+const session = require("express-session"); 
+// Requiring passport as we've configured it
+const passport = require("./config/passport"); 
+const fileupload = require("express-fileupload"); 
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 3001; 
 
 const db = require("./models");
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3001"
-};
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
 
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to CRUD application." });
-});
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(fileupload());
 
 
 app.use(routes);
-// set port, listen for requests
-const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
