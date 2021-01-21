3852314a16dc27edc366c34bbcc215679569ec0c
Cesar Martinez <cesaguma@hotmail.com>
2021-01-20 22:10:51 -0500

    Adding the route for meeting.js file.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,13 @@ const companyRoutes = require("./companies");
 const categoryRoutes = require("./categories");
 const adRoutes = require("./ads");
 const userRoutes = require("./users");
+const meetRoutes = require("./meetings");
 
 // Company routes
 router.use("/companies", companyRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/ads", adRoutes);
-router.use("/users", userRoutes)
+router.use("/users", userRoutes);
+router.use("/meetings", meetRoutes);
 
 module.exports = router;
