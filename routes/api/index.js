a5decd8162a81b9c4cd97bb7294924007887a161
Cesar Martinez <cesaguma@hotmail.com>
2021-01-20 21:51:38 -0500

    Updating routes into index.js file in the routes/api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const companyRoutes = require("./companies");
+const categoryRoutes = require("./categories");
+const adRoutes = require("./ads");
+const userRoutes = require("./users");
 
 // Company routes
 router.use("/companies", companyRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/ads", adRoutes);
+router.use("/users", userRoutes)
 
 module.exports = router;
