13d782c898fd5531eb2e284c4bb67e523c910d6f
Cesar Martinez <cesaguma@hotmail.com>
2021-01-22 02:14:02 -0500

    Merging Login and Register into one component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
-import RegistrationForm from "./pages/RegistrationForm/RegirtrationForm";
+// import RegistrationForm from "./pages/RegistrationForm/RegirtrationForm";
 
 // import LoginForm from "./pages/LoginForm/LoginForm";
 import LoginRegister from "./pages/LoginRegister/LoginRegister";
 import "./App.css";
 
-import LoginForm from "./pages/LoginForm/LoginForm";
+// import LoginForm from "./pages/LoginForm/LoginForm";
 import { ProtectedRoute } from "./protected.route";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -20,8 +20,8 @@ function App() {
         <Nav />
           <div className="container d-flex align-items-center flex-column">         
             <Switch>
-              <Route exact path="/register" component={RegistrationForm}/>
-              <Route exact path="/login" component={LoginForm}/>
+              {/* <Route exact path="/register" component={RegistrationForm}/> */}
+              {/* <Route exact path="/login" component={LoginForm}/> */}
               <Route exact path="/" component={Home}/>
               <Route exact path="/login" >
                 <LoginRegister />
