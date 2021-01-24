8202160f9473eac93f604f01b499a636777919f4
Cesar Martinez <cesaguma@hotmail.com>
2021-01-24 01:17:25 -0500

    Creating a USERID variable to control user's access

--- client/src/constants/apiConstants.js
+++ client/src/constants/apiConstants.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
 export const ACCESS_TOKEN_NAME = 'login_access_token';
 export const ACCESS_AUTHENTICATED = 'Authenticated';
+export const USERID = "UserId";
