2605aa0e0f7459eb86e44b98584a5d15baf3a733
Cesar Martinez <cesaguma@hotmail.com>
2021-01-27 18:44:10 -0500

    Adding the new components Navbar and Container with their parameters inside of the DashboardHome component.

--- client/src/pages/Dashboard/DashboardHome.js
+++ client/src/pages/Dashboard/DashboardHome.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState} from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Container from "../../components/Container";
 
 function DashboardHome() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
-      <Navbar />
+      <Navbar sidebar={sidebar} isActive={showSidebar}/>
+      <Container sidebar={sidebar} isActive={showSidebar} style={{ marginTop: 30 }}>
       <div className="dashboardHome">
         <h1>Dashboard Home</h1>
       </div>
+      </Container>      
     </>
   );
 }
