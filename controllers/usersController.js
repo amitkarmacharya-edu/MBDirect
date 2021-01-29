8a7e93f444837dec0f1a135d9a0381842881a9d4
Cesar Martinez <chernanma@msn.com>
2021-01-28 21:47:33 -0500

    Updating user controller for updating action

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,48 +1,68 @@
 const db = require("../models");
+const bcrypt = require("bcryptjs");
 
 // Defining methods for the UsersController
 module.exports = {
-  findAll: function(req, res) {
-    db.User
-      .findAll({include: [db.Company]})     
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
+  findAll: function (req, res) {
+    db.User.findAll({ include: [db.Company] })
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
   },
-  findById: function(req, res) {
-    db.User
-      .findOne({
+  findById: function (req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [db.Company],
+    })
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.User.create(req.body)
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    idToNumber = parseInt(req.params.id);
+    let passwordUpdated = req.body.password;
+    if (passwordUpdated !== undefined ) {
+      if (passwordUpdated.length <= 20) {
+        passwordUpdated = bcrypt.hashSync(
+          passwordUpdated,
+          bcrypt.genSaltSync(10),
+          null
+        );
+      }
+    }
+    db.User.update(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        zip_code: req.body.zip_code,
+        country: req.body.country,
+        password: passwordUpdated,
+      },
+      {
         where: {
-          id: req.params.id
+          id: idToNumber,
         },
-        include: [db.Company]
-      })
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
+      }
+    )
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
   },
-  create: function(req, res) {
-    db.User
-      .create(req.body)
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
+  remove: function (req, res) {
+    db.User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbUser) => res.json(dbUser))
+      .catch((err) => res.status(422).json(err));
   },
-  update: function(req, res) {
-    db.User
-      .Update(req.body, {
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.User
-      .destroy({
-        where: {
-          id: req.params.id
-        }
-      })      
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
-  }
 };
