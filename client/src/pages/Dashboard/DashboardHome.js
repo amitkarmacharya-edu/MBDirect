1520b07dd35c2c9f3784b3f7ba609770c8e44d74
Cesar Martinez <cesaguma@hotmail.com>
2021-01-29 17:55:10 -0500

    Adding Navbar  and Container components.

--- client/src/pages/Dashboard/DashboardHome.js
+++ client/src/pages/Dashboard/DashboardHome.js
@@ -1,14 +1,28 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Container from "../../components/Container";
+
+
 
 function DashboardHome() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
+  
+
+  useEffect(() => {
+   
+  }, [])
+
+  
+
   return (
     <>
-      <Navbar sidebar={sidebar} isActive={showSidebar}/>
-      <Container sidebar={sidebar} isActive={showSidebar} style={{ marginTop: 30 }}>
+       <Navbar sidebar={sidebar} isActive={showSidebar} />
+      <Container
+        sidebar={sidebar}
+        isActive={showSidebar}
+        style={{ marginTop: 30 }}
+      >
       <div className="dashboardHome">
         <h1>Dashboard Home</h1>
       </div>
