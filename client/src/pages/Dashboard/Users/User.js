e5920c4db9f396411661661a3dc20d36295e5515
Cesar Martinez <chernanma@msn.com>
2021-01-28 21:04:32 -0500

    Moving deleteUser function from CardUser.js to User.js

--- client/src/pages/Dashboard/Users/User.js
+++ client/src/pages/Dashboard/Users/User.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import Container from "../../../components/Container";
+import { Alert } from "../../../components/AlertComponent/Alert";
 import API from "../../../utils/API";
 import SearchForm from "../../../components/SearchForm";
 import SearchResults from "../../../components/SearchResults";
@@ -9,22 +10,22 @@ import Row from "../../../components/Row";
 import Col from "../../../components/Col";
 import { USERID } from "../../../constants/apiConstants";
 
-
-
 function User({ match }, props) {
   const { path } = match;
   const [users, setUsers] = useState([]);
   const [userType, setUserType] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userDeleted, setUserDeleted] = useState(false);
   const [filteredUsers, setFilteredUsers] = useState([users]);
   const [sortType, setSortType] = useState("a-z");
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
 
-
   useEffect(() => {
+    setUserDeleted(false);
     loadUsers();
     typeUsers();
-  }, []);
+  }, [userDeleted]);
 
   function loadUsers() {
     API.getUsers()
@@ -41,6 +42,7 @@ function User({ match }, props) {
     API.getUser(userId).then((res) => {
       console.log(res.data.type);
       setUserType(res.data.type);
+      setUserName(res.data.first_name + " " + res.data.last_name);
     });
   }
 
@@ -76,37 +78,63 @@ function User({ match }, props) {
     return setFilteredUsers(listUsersSorted);
   }
 
+  function deleteUser (e) {
+    e.preventDefault();
+    API.deleteUser(e.target.id)
+      .then(function (response) {
+        console.log(response);
+        return setUserDeleted(true);
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+  }
+
   return (
     <div>
-      <Navbar sidebar={sidebar} isActive={showSidebar}/>      
-      <Container sidebar={sidebar} isActive={showSidebar} style={{ marginTop: 30 }}>     
-     
+      <Navbar sidebar={sidebar} isActive={showSidebar} userName={userName}/>
+      {/* <Alert /> */}
+      <Container
+        sidebar={sidebar}
+        isActive={showSidebar}
+        style={{ marginTop: 30 }}
+      >
         <Row>
           <Col size="md-12">
             <div>
               <Container style={{ minHeight: "80%" }}>
-                <h1 style={{ color: "white" }}>Users</h1>
-                <Link
-                  to={`${path}/add`}
-                  className="btn btn-sm btn-success mb-2"
-                >
-                  Add User
-                </Link>                
-                <SearchForm
-                  pageName="Users"
-                  handleInputChange={handleInputChange}
-                  sortBy={sortBy}
-                />
+                           
+                {userType === "Admin" ? (
+                  <>
+                    <Link
+                      to={`${path}/add`}
+                      className="btn btn-sm btn-success mb-2"
+                    >
+                      Add User
+                    </Link>
+                    <SearchForm
+                      pageName="Users"
+                      handleInputChange={handleInputChange}
+                      sortBy={sortBy}
+                      
+                    />
+                  </>
+                ) : (
+                  <></>
+                )}
+
                 <SearchResults
                   results={filteredUsers}
                   userType={userType}
+                  key={USERID}
                   pageName="Users"
+                  deleteUser = {deleteUser}
                 />
               </Container>
             </div>
           </Col>
         </Row>
-        </Container>     
+      </Container>
     </div>
   );
 }
