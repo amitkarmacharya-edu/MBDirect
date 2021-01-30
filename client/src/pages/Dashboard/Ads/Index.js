73f2f6dbacb0b1cdfd8d11ecc8856c9b074259f6
Cesar Martinez <cesaguma@hotmail.com>
2021-01-30 02:17:36 -0500

    Activating the routes to update and create ads.

--- client/src/pages/Dashboard/Ads/Index.js
+++ client/src/pages/Dashboard/Ads/Index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Ad } from "./Ad";
-// import { AddEditAd } from "./AddEditAd";
+import { AddEditAd } from "./AddEditAd";
 
 
 function Ads({ match }) {
@@ -12,8 +12,8 @@ function Ads({ match }) {
       
         <Switch>
           <Route exact path={path} component={Ad} />
-          {/* <Route exact path={`${path}/add`} component={AddEditAd} /> */}
-          {/* <Route exact path={`${path}/edit/:id`} component={AddEditAd} /> */}
+          <Route exact path={`${path}/add`} component={AddEditAd} />
+          <Route exact path={`${path}/edit/:id`} component={AddEditAd} />
         </Switch>
       
     </div>
