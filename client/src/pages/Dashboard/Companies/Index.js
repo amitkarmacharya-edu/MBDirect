109968fea5d6424d34e1782efb869f2513dbd611
Cesar Martinez <chernanma@msn.com>
2021-01-29 22:23:29 -0500

    Add Route for AddEdit component in Index.js

--- client/src/pages/Dashboard/Companies/Index.js
+++ client/src/pages/Dashboard/Companies/Index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Company } from "./Company";
-// import { AddEditCompany } from "./AddEditCompany";
+import { AddEditCompany } from "./AddEditCompany";
 
 
 function Companies({ match }) {
@@ -12,8 +12,8 @@ function Companies({ match }) {
       
         <Switch>
           <Route exact path={path} component={Company} />
-          {/* <Route exact path={`${path}/add`} component={AddEditCompany} /> */}
-          {/* <Route exact path={`${path}/edit/:id`} component={AddEditComany} /> */}
+          <Route exact path={`${path}/add`} component={AddEditCompany} />
+          <Route exact path={`${path}/edit/:id`} component={AddEditCompany} />
         </Switch>
       
     </div>
