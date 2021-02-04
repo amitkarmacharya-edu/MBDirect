8264ebf46e382f49b4e32c26f01974044d6d8038
Adrian Storr <Adrianloves123@gmail.com>
2021-02-04 00:07:53 -0500

    added Footer components

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Footer from "../../components/Footer";
+import Main from "../../components/Main";
+import Header from "../../components/Header";
 function Home() {
 
  
