d03a4f43b117d481ed09034fa953ece73b620a8c
Cesar Martinez <chernanma@msn.com>
2021-02-05 14:35:51 -0500

    Adding alt prop to img elements

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import logo from "../../Images/MBDIRECT-Pagr.png";
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand text-danger m-auto" href="/">MBDirect</a>
+            <a className="navbar-brand text-danger m-auto" href="/"><img className="navbar-brand" src={logo} alt="logo" width="140" height="60" /></a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
