da79ff60f22add3fa3789e1461b3f78084203539
amit karmacharya <amitkarmacharya@MacBook-Pro.local>
2021-02-04 22:56:48 -0500

    saved prop to save peer connectiona nd added event listener on the peerconnection object

--- client/src/utils/globalState/webrtc/webrtc-peerConnections.js
+++ client/src/utils/globalState/webrtc/webrtc-peerConnections.js
@@ -28,12 +28,21 @@ class WebRTCPeerConnection {
 
         // RTCPeerConnection creation and initialization
         this.pc = new RTCPeerConnection(this.rtcConfig);
-
+        this.pc.onnegotiationneeded = this.onnegotiation.bind(this);
+        this.pc.onicecandidate = this.onIceCandidate.bind(this);
+        this.pc.oniceconnectionstatechange = this.onConnectionStateChange.bind(this);
+        this.pc.onicecandidateerror = this.onIceCandidateError;
+        this.pc.ontrack = this.receivedRemoteStream.bind(this);
+        this.pc.onconnectionstatechange = this.connectionStateChange.bind(this);
+        this.pc.onicegatheringstatechange = this.iceGatheringState;
+        
         // callbacks to handle various RTCPeeconnection event
         // will be initialized by the app layer
         this.onErrorCB = errorHandler;
         this.onRemoteStreamCB = remoteStreamHandler;
         this.onSignaler = signalHandler;
+
+
 
     }
 
