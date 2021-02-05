a36b44a140dee64b778633b5f2f2245827cb8d95
amit karmacharya <amitkarmacharya@MacBook-Pro.local>
2021-02-04 22:19:40 -0500

    code refactor on globalstate

--- client/src/utils/globalState/webrtc-globalState.js
+++ client/src/utils/globalState/webrtc-globalState.js
@@ -1,123 +1,121 @@
 import React, { createContext, useReducer, useContext } from "react";
 import {
-  SET_USER_ID,
-  SAVE_USER_INFO,
-  CLEAR_USER_INFO,
-  START_MEETING,
-  CLOSE_MEETING,
-  UPDATE_STAGE,
-  SHOW_ALERTS,
-  HIDE_ALERTS,
-  LOADING
+    SET_USER_ID,
+    SAVE_USER_INFO,
+    CLEAR_USER_INFO,
+    START_MEETING,
+    CLOSE_MEETING,
+    UPDATE_STAGE,
+    SHOW_ALERTS,
+    HIDE_ALERTS,
+    LOADING
 } from "./actions";
 
 const MeetingContext = createContext();
 const { Provider } = MeetingContext;
 
 const reducer = (state, action) => {
-    
-    switch(action.type){
+
+    switch (action.type) {
+
         case SET_USER_ID:
             return {
-        ...state,
-        userId: action.userId,
-        loading: false
-      };
+                ...state,
+                userId: action.userId,
+                loading: false
+            };
 
-      case SAVE_USER_INFO:
-      return {
-        ...state,
-        lobby: action.lobby,
-        loading: false
-      };
+        case SAVE_USER_INFO:
+            return {
+                ...state,
+                lobby: action.lobby,
+                loading: false
+            };
 
-      case CLEAR_USER_INFO:
-      return {
-        ...state,
-        lobby: {
-          firstName: "",
-          lastName: "",
-          email: "",
-          phoneNumber: "",
-          subject: "",
-          description: "",
-        },
-        loading: false
-      };
+        case CLEAR_USER_INFO:
+            return {
+                ...state,
+                lobby: {
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    phoneNumber: "",
+                    subject: "",
+                    description: "",
+                },
+                loading: false
+            };
 
-      case START_MEETING:
-      return {
-        ...state,
-        roomId: action.roomId,
-        businessId: action.businessId,
-        roomCreator: action.roomCreator,
-        meetingStarted: true,
-        loading: false,
-        meetingType: action.meetingType
-      };
+        case START_MEETING:
+            return {
+                ...state,
+                roomId: action.roomId,
+                businessId: action.businessId,
+                roomCreator: action.roomCreator,
+                meetingStarted: true,
+                loading: false,
+                meetingType: action.meetingType
+            };
 
-      case CLOSE_MEETING:
-        return {
-          ...state,
-          lobby:{
-            ...state.lobby,
-            subject: "",
-            description: ""
-          },
-          roomId: "",
-          businessId: "",
-          currentStage: state.stages[0],
-          currentStageIndex: 0,
-          roomCreator: false,
-          meetingStarted: false,
-          loading: false
-        };
-  
+        case CLOSE_MEETING:
+            return {
+                ...state,
+                lobby: {
+                    ...state.lobby,
+                    subject: "",
+                    description: ""
+                },
+                roomId: "",
+                businessId: "",
+                currentStage: state.stages[0],
+                currentStageIndex: 0,
+                roomCreator: false,
+                meetingStarted: false,
+                loading: false
+            };
+
         case UPDATE_STAGE:
             return {
-              ...state,
-              currentStage: action.updatedStage,
-              currentStageIndex: action.updatedStageIndex,
-              loading: false,
+                ...state,
+                currentStage: action.updatedStage,
+                currentStageIndex: action.updatedStageIndex,
+                loading: false,
             };
-      
-            case SHOW_ALERTS:
-                return {
-                  ...state,
-                  alertMsgs: action.errMsg,
-                  showAlerts: true
-                };
-          
-                case HIDE_ALERTS:
-                    return {
-                      ...state,
-                      showAlerts: false,
-                    };
-              
-                    case LOADING:
-      return {
-        ...state,
-        loading: true
-      };
 
+        case SHOW_ALERTS:
+            return {
+                ...state,
+                alertMsgs: action.errMsg,
+                showAlerts: true
+            };
 
-      default:
-        return state;
+        case HIDE_ALERTS:
+            return {
+                ...state,
+                showAlerts: false,
+            };
 
+        case LOADING:
+            return {
+                ...state,
+                loading: true
+            };
+
+        default:
+            return state;
     }
-
 };
 
 const MeetingProvider = ({ value = [], ...props }) => {
-    
+
     const [state, dispatch] = useReducer(reducer, {
         lobby: {
-          firtName: "",
-          lastName: "",
-          email: "",
-          phoneNumber: "",
-          subject: "",
-          message: ""
+            firtName: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            subject: "",
+            message: ""
         },
         alertMsgs: "",
         showAlerts: false,
@@ -131,7 +129,7 @@ const MeetingProvider = ({ value = [], ...props }) => {
         meetingStarted: false,
         loading: false,
         meetingType: ""
-      });
+    });
 
     return <Provider value={[state, dispatch]} {...props} />;
 };
