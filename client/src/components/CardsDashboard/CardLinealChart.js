a99712e63f6da51da211e4acd9ad1de37fdcb2d3
Cesar Martinez <cesaguma@hotmail.com>
2021-02-06 19:17:50 -0500

    Adding API calls to get data from the database to be used into the lineal chart on dashboard page.

--- client/src/components/CardsDashboard/CardLinealChart.js
+++ client/src/components/CardsDashboard/CardLinealChart.js
@@ -3,28 +3,52 @@ import Chart from "react-google-charts";
 import * as FcIcons from "react-icons/fc";
 import { Card } from "react-bootstrap";
 import API from "../../utils/API";
+import { USERID } from "../../constants/apiConstants";
 
-function CardLinealChart() {
+
+function CardLinealChart(props) {
   const [chartData, setChartData] = useState();
+  const [userType, setUserType] = useState("");
 
-  useEffect(() => {
-    countCategories();
-    console.log(chartData);
-  }, []);
 
-  function countCategories() {
-    let categoriesName = [["Categories", "# Companies"]];
-    API.countCategoriesByCompany()
+  useEffect(() => { 
+    typeUsers();  
+    countMeets();
+  }, [userType]);
+  
+  function typeUsers() {
+    const userId = localStorage.getItem(USERID);
+    API.getUser(userId).then((res) => {
+      setUserType(res.data.type);
+    });
+  }
+
+  function countMeets() {
+    let meetsMonths = [["Month", "# Meets"]];
+    let date = new Date();
+    let year = date.getFullYear();
+    if (userType === "Owner") {
+      API.countMeetsByMonth(localStorage.getItem(USERID),year)
       .then((res) => {
         res.data.forEach((element) => {
-          categoriesName.push([element.name, element.numberCompanies]);
+          meetsMonths.push([element.MonthName, element.Meets]);
         });
       })
       .catch((err) => console.log(err));
-    console.log(categoriesName);
-    setChartData(categoriesName);
-    console.log(chartData);
+    } else if (userType === "Admin"){
+      API.countAllMeetsByMonth(year)
+      .then((res) => {
+        res.data.forEach((element) => {
+          meetsMonths.push([element.MonthName, element.Meets]);
+        });
+      })
+      .catch((err) => console.log(err));
+    } 
+    
+    setChartData(meetsMonths);
   }
+
+
 
   return (
     <>
@@ -38,28 +62,16 @@ function CardLinealChart() {
         <Card.Body style={{ overflow: "auto" }}>
           <div style={{ display: "flex", maxWidth: 900 }}>
             <Chart
-              width={"600px"}
-              height={"400px"}
+              width={"100%"}
+              height={"auto"}
               chartType="ComboChart"
               loader={<div>Loading Chart</div>}
-              data={[
-                ["Month", "Meets"],
-                ["Jan", 12],
-                ["Feb", 5.5],
-                ["Mar", 30],
-                ["Apr", 5],
-                ["May", 3.5],
-                ["Jul", 7],
-                ["Agu", 7],
-                ["Sep", 7],
-                ["Oct", 7],
-                ["Nov", 7],
-                ["Dec", 7],
-              ]}
+              data={chartData}
               options={{
                 hAxis: { title: "Months"},
                 vAxis: { title: "Meets"},
                 legend: "none",
+                chartArea:{width: "70%", height: "70%"},
                 seriesType: "bars",           
               }}
               rootProps={{ "data-testid": "1" }}
@@ -70,7 +82,7 @@ function CardLinealChart() {
         <Card.Footer>
           <hr />
           <div className="stats">
-            <FcIcons.FcCalendar /> Last Month
+            <FcIcons.FcBarChart /> Lineal Bar Chart
           </div>
         </Card.Footer>
       </Card>
