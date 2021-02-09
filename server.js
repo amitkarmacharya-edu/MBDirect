f0cc7d541ac405d24bd2aa2d4cf413be790df320
amitkarmacharya-edu <amit.education2018@gmail.com>
2021-02-08 22:55:45 -0500

    added socket.io in server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
+const { v4: uuidv4 } = require('uuid');
 
 const session = require("express-session"); 
 // Requiring passport as we've configured it
@@ -11,10 +13,19 @@ const isAuthenticated = require("./config/middleware/isAuthenticated");
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3001; 
 
+const app = express();
+const http= require("http").Server(app);
+const io = require("socket.io")(http, {
+  cookie: true
+});
 
+// Define middleware here
+if(process.env.NODE_ENV !== "production"){
+  app.options("*", cors());
+  app.use(cors());
+}
 
 const db = require("./models");
-const app = express();
 app.use(fileupload());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
@@ -55,10 +66,15 @@ app.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
+// creates a UUID for the user to use in meets
+app.get("/meetsId", (req,res) => {
+  res.status(200).json(uuidv4());
+})
+
 app.use(routes);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  http.listen(PORT, () => {
     console.log(
       "==> :earth_americas:  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
@@ -66,3 +82,169 @@ db.sequelize.sync().then(() => {
     );
   });
 });
+
+
+// stores relations between user and business
+// store all the business ids, users send request
+// to business using their ids
+const bidToSid = {}; // bid to business user socket it, to forward the messages
+
+// socket
+io.on('connection', (socket) => {
+
+  console.log("user connected");
+
+  log(socket.id);
+
+  // creates rooms based on the business owner socket id
+  socket.on("RegisterBusinessToMeets", async ({userId}) => {
+    log("ResterBusinessToMeets");
+    log("userID: " + userId);
+    log("socketID : " + socket.id);
+    // check if the owner has a business associated to him
+    const companies = await db.Company.findAll({
+      where: {UserId: userId},
+      raw: true
+    });
+
+    log("company List");
+    log(companies);
+    log(companies.length === 0);
+    // if there are no business under the user name
+    if(companies.length === 0){
+      socket.emit("FailedToRegisterBusiness", "No company Registered to the userID");
+      return;
+    }
+
+    // for each company map the business id to the ownders active socket it
+    companies.forEach(company => {
+      const bId = company.id;
+      // check if the business is already registered to Meets
+      if(Object.keys(bidToSid).length > 0 && bidToSid[bId]) {
+        socket.emit("FailedToRegisterToMeets", "Bad Request: Business Already connected to Meets");
+        return;
+      }
+
+      // add the business map to owner socket it
+      // all the connection to the business will be forwarded to the users/owner.
+      log(bId);
+      bidToSid[bId] = socket.id;
+    });
+    
+    log(bidToSid);
+  });
+
+  /**
+   * Connecting to Business room
+   */
+
+  // register the client to the room with business
+  socket.on("connectToBusiness", ({userInfo, meetingType, bId, userId}) => {
+    log("connecting to business");
+    log("BusinessId : " + bId);
+    log("userID : " + userId);
+    log(userInfo);
+    log("Business online: " + (bidToSid[bId] && true));
+    log("Business socket: " + bidToSid[bId]);
+    log(bidToSid);
+
+    if(!bId || !userId){
+      socket.emit("failedToConnectBusiness", {error: "Failed: \
+      business Id or client id was not provided"});
+      return;
+    }
+
+    if(!bidToSid[bId]) {
+      console.log("failed to connect");
+      socket.emit("failedToConnectBusiness", {error: "failed: \
+      Business with that id is not online"});
+      return;
+    }
+
+    // // no more than 2 people in one room
+    // if (io.sockets.adapter.rooms.get(bidToSid[bId]).size >= 2){
+    //   socket.emit("BusinessBusy", {errMsg: "Business is Busy with another caller"});
+    //   return;
+    // }
+
+    // join room
+    console.log("connected to business: " + bId);
+    
+    socket.join(bidToSid[bId]);
+
+    socket.to(bidToSid[bId]).emit("dialTone", {
+      userInfo: userInfo,
+      meetingType: meetingType,
+      remoteSocketId: socket.id,
+      remoteUserId: userId
+    });
+
+  });
+
+  // leave room
+  socket.on("leaveRoom", ({businessSocketId}) => {
+    console.log("leaveRoom");
+    socket.leave(businessSocketId);
+  });
+
+
+  /**
+   * Incoming and outgoing calls
+   */
+
+  // // dial tone
+  // this.socket.on("dialTone", ({remoteSocketId, msg}) => {
+  //   console.log(msg);
+  // });
+
+  // call rejected
+  socket.on("callRejected", ({remoteSocketId}) => {
+      console.log("Call rejected for : " + remoteSocketId);
+      socket.to(remoteSocketId).emit("callRejected", {remoteUserId: socket.id});
+  });
+
+  // // call accepted
+  // this.socket.on("callAccepted", () => {
+  //     console.log("Call was accepted");
+  // });
+
+
+  /** 
+   * webrtc
+   * */
+  
+  // sdp description
+  socket.on("signalChannel", ({remoteSocketId, userId, data}) => {
+    console.log("broadCast sdp and candidates");
+    console.log("broadcast TYPE: " + data.peerData.type);
+    socket.to(remoteSocketId).emit("peerMessages", {
+      remoteSocketId: this.remoteSocketId, 
+      userId: userId,
+      data: data
+    });
+  });
+  
+  // on disconnect, clear the business to owner map if it exits
+  socket.on("disconnect", () => {
+    console.log(socket.id);
+    console.log("disconnect");
+    if(bidToSid)
+    {
+      const bId = Object.keys(bidToSid).filter(bId =>  bidToSid[bId] === socket.id);
+      log("bid")
+      log(bId);
+      if(bId.length > 0) {
+        bId.forEach(bId => delete bidToSid[bId]);
+      }
+    }
+
+    log(bidToSid);
+  });
+
+});
+
+function log(v){
+  console.log("-----------------");
+  console.log(v);
+  console.log("-----------------");
+}
