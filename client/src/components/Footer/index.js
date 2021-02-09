3803b68eb77c468501949f35fb74e738ddde247d
Cesar Martinez <cesaguma@hotmail.com>
2021-02-09 02:33:35 -0500

    Adding icons, links, and styles to the footer element.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,31 +2,36 @@ import React from "react";
 import "./style.css";
 function Footer() {
     return(
-        <footer className="container-fluid border pt-5">
-            <div className="row">
-                <div className="col-sm-10 col-md-8 m-auto">
-                    <h5>Questions?
-                        <a href="tel:+62896706255135"> Call us at +62-896-7062-55135</a>
-                    </h5>
-                    <div className="d-flex flex-wrap justify-content-between">
-                        <a href="/" className="m-2">FAQ</a>
-                        <a href="/" className="m-2">Help Center</a>
-                        <a href="/" className="m-2">Terms of Use</a>
-                        <a href="/" className="m-2">Privacy</a>
-                        <a href="/" className="m-2">Cookie Perference</a>
-                    </div>
-                    {/* social media */}
-                    <div className="d-flex flex-wrap justify-content-end mt-2">
-                    <a href="#" className="fa fa-facebook m-1"></a>
-                    <a href="#" className="fa fa-twitter m-1"></a>
-                    <a href="#" className="fa fa-youtube m-1"></a>
-                    <a href="#" className="fa fa-instagram m-1"></a>
+        <footer className="container-footer">
+            <div className="footer-wrapper">
+                <div className="row">
+                    <div className="col-sm-10 col-md-8 m-auto">
+                        <h6>Questions?     
+                        <a className="m-2" href="tel:+12023062469">Call us at +1-202-306-2469</a>
+                        </h6>
+                        <br></br>
+                        <div className="d-flex flex-wrap justify-content-between">
+                            <a href="#" className="m-2">FAQ</a>
+                            <a href="#" className="m-2">Help Center</a>
+                            <a href="#" className="m-2">Terms of Use</a>
+                            <a href="#" className="m-2">Privacy</a>
+                            <a href="#" className="m-2">Cookie Perference</a>
+                        </div>
+                        {/* social media */}
+                        <div className="d-flex flex-wrap justify-content-center mt-4">
+                        <a href="#" className="fa fa-facebook fa-2x mb-2 mr-4"></a>
+                        <a href="#" className="fa fa-twitter fa-2x mb-2 mr-4"></a>
+                        <a href="https://github.com/chernanma/MBDirect" target="_blank" className="fa fa-github fa-2x mb-2 mr-4"></a>
+                        <a href="#" className="fa fa-instagram fa-2x mb-2 mr-4"></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12 border p-2 text-center mt-2">
-                    2021 copyright
+                <div className="row">
+                <div class="col-md-8 col-xs-12 offset-md-2">
+                    <div className="panel-footer">
+                    ©2020 - 2021 MB Direct - Inter TEAM. All Rights Reserved.
+                    </div>
+                    </div>
                 </div>
             </div>
         </footer>
