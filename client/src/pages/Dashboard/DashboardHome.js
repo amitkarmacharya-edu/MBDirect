f2f4f1baf748d20bb127ccc59467a4fe5d11b307
amitkarmacharya-edu <amit.education2018@gmail.com>
2021-02-08 20:46:42 -0500

    updated dashboard and home js, included meeting / incoming component

--- client/src/pages/Dashboard/DashboardHome.js
+++ client/src/pages/Dashboard/DashboardHome.js
@@ -1,35 +1,79 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Container from "../../components/Container";
+import MeetingRoom from "../../components/MeetingRoom";
+import IncomingCall from "../../components/IncomingCall";
 
-
+import meap from "../../utils/webrtc/webrtc-meap";
+import { CLOSE_MEETING, INCOMING_CALL, SAVE_USER_INFO, SET_USER_ID, SHOW_ALERTS } from "../../utils/globalState/webrtc/actions";
+import { useMeetingContext } from "../../utils/globalState/webrtc/webrtc-globalState";
 
 function DashboardHome() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
-  
+
+  const [state, dispatch] = useMeetingContext();
 
   useEffect(() => {
-   
+    // check for user id
+    if (localStorage.getItem("UserId") && localStorage.getItem("Authenticated") === "true") {
+      console.log("LocalStorage userId : " + localStorage.getItem("UserId"));
+      meap.userId = localStorage.getItem("UserId");
+      dispatch({type: SET_USER_ID, userId: localStorage.getItem("UserId")});
+    }
+
+    if (!meap.signalingChannel) {
+      meap.createSocketConnection(handleDialTone, callGotRejected)
+        .then(() => {
+          console.log(meap);
+        });
+    }
   }, [])
 
-  
+  function handleDialTone(data) {
+      console.log("coming from handleDiatone inside Dashboard");
+        console.log(data);
+        dispatch({ type: INCOMING_CALL, data: data });
+        console.log(state);
+        alert();
+  }
+
+  function callGotRejected() {
+    console.log("call got rejected by the business");
+    dispatch({type: SHOW_ALERTS, errMsg: "Business is busy with another client."});
+    dispatch({type: CLOSE_MEETING});
+  }
+
+  function answerCall(remoteSocketID) {
+    console.log("Business Answered call");
+  }
 
   return (
     <>
-       <Navbar sidebar={sidebar} isActive={showSidebar} />
+      <Navbar sidebar={sidebar} isActive={showSidebar} />
       <Container
         sidebar={sidebar}
         isActive={showSidebar}
         style={{ marginTop: 30 }}
       >
-      <div className="dashboardHome">
-        <h1>Dashboard Home</h1>
-      </div>
-      </Container>      
+        <div className="dashboardHome">
+          <h1>Dashboard Home</h1>
+        </div>
+      </Container>
+
+      {state.meetingStarted &&
+        <MeetingRoom
+          handleDialTone={handleDialTone}
+          callGotRejected={callGotRejected}
+        />}
+
+      {state.showCallNotification &&
+        <IncomingCall 
+          handleDialTone={handleDialTone}
+          callGotRejected={callGotRejected}
+        />}
     </>
   );
 }
 
 export default DashboardHome;
-
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Jumbo from "../../components/Jumbo";
-
-
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import Footer from "../../components/Footer";
+import Jumbo from "../../components/Jumbo";
+import IncomingCall from "../../components/IncomingCall";
+import MeetingRoom from "../../components/MeetingRoom";
+import { SHOW_ALERTS, CLOSE_MEETING } from "../../utils/globalState/webrtc/actions";
+import { useMeetingContext } from "../../utils/globalState/webrtc/webrtc-globalState";
+import meap from "../../utils/webrtc/webrtc-meap";
 
 function Home() {
 
- 
+  const [state, dispatch] = useMeetingContext();
 
-    return(
-<><Header/>
-<Jumbo/>
-<Footer/></>
-        
+  function callGotRejected() {
+    console.log("call got rejected by the business");
+    dispatch({type: SHOW_ALERTS, errMsg: "Business is busy with another client."});
+    dispatch({type: CLOSE_MEETING});
+  }
 
-    )
+  return (
+    <>
+      <Header />
+      <Jumbo />
+      <Main />
+      {state.meetingStarted && <MeetingRoom 
+          callGotRejected={callGotRejected}
+          />}
+      {state.showCallNotification && <IncomingCall />}
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
