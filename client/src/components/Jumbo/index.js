b6cc8eadf517c9a6295c88baf84c9a4a2ca030f5
Cesar Martinez <chernanma@msn.com>
2021-02-09 20:22:34 -0500

    Adding Carousel to Main page for Ads

--- client/src/components/Jumbo/index.js
+++ client/src/components/Jumbo/index.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import Carousel from "react-bootstrap/Carousel";
+import API from "../../utils/API";
+
 function Jumbo() {
+  const [adsData, setAdsData] = useState([]);
+
+  useEffect(() => {
+    loadAds();
+  }, []);
+
+  function loadAds() {
+    let ads = [];
+    API.getAds().then((res) => {
+      if (res.data.length !== 0) {
+        for (let i = 0; i < res.data.length; i++) {
+          if (i <= 5) {
+            ads.push(res.data[i]);
+          }
+        }
+      }
+      setAdsData(ads);
+      console.log(ads);
+    });
+  }
+
   return (
     <div className="container-search">
-      <div className="search-wrapper">        
-        <input name="company" type="text" className="form-control" placeholder="Type Company Name to Search" id="company"></input>
+      <div className="search-wrapper">
+        <Carousel>
+          {adsData.map((res) => (
+            <Carousel.Item interval={1000}>
+              <div className="adimage col-md-8 offset-md-2">
+                <img className="banner" src={res.image} alt={res.id} />
+              </div>
+              <Carousel.Caption className="captions" style={{paddingTop: "5px"}}>
+                <h3>{res.name}</h3>
+                <p>{res.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
