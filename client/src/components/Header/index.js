e259bae7e2147b0bc01ee5f85c6c602199ee3bee
Cesar Martinez <chernanma@msn.com>
2021-02-10 13:08:56 -0500

    Adding About Us page to WebApp

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -14,11 +14,11 @@ function Header() {
                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">About</a>
+                        <a className="nav-link" href="/about">About</a>
                     </li>
-                    <li className="nav-item">
+                    {/* <li className="nav-item">
                         <a className="nav-link" href="/">For Business</a>
-                    </li>
+                    </li> */}
                     <li className="nav-item">
                     <Link className="nav-link" to="/login" style={{color: "red", verticalAlign:"middle"}}> Login</Link>
                     </li>
